7b6bd470e3234cd34dae44bf562a9b01906997a3
peihanWang <nocab7714@gmail.com>
2023-08-20 09:43:58 +0800

    fix : 願望清單的按鈕 html 結構

--- src/views/fronted/ProductsView.vue
+++ src/views/fronted/ProductsView.vue
@@ -80,8 +80,7 @@
             <div class="card border-0 h-100 position-relative">
               <!-- 加入許願清單按鈕 -->
               <a class="wishLists-btn fs-4 link-light" href="" @click.prevent="addWishList(product)"
-                ><i class="bi bi-heart position-relative"></i
-                ><i class="bi bi-heart-fill position-absolute"></i
+                ><i class="bi bi-heart"></i
               ></a>
               <router-link class="product-link text-decoration-none" :to="`/product/${product.id}`">
                 <div class="position-relative">
@@ -365,17 +364,5 @@ export default {
   z-index: 4;
   right: 10px;
   top: 5px;
-}
-
-.bi-heart-fill {
-  left: 0;
-  opacity: 0;
-  &:hover {
-    opacity: 100;
-  }
-  &:active {
-    opacity: 100;
-    color: $pink;
-  }
 }
 </style>
