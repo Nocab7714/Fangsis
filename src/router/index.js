973ada79b8283846eab34c6d69247a5e7dbca6be
peihanWang <nocab7714@gmail.com>
2023-08-27 21:48:53 +0800

    fix : 調整路由表
    1.移除所有路由的 name 屬性
    2.設定 CartCompleteOrder 動態路由設定
    3.新增 PayOrder 動態路由設定

--- src/router/index.js
+++ src/router/index.js
@@ -16,90 +16,65 @@ const router = createRouter({
         },
         {
           path: 'about',
-          name: 'about',
           component: () => import('../views/fronted/AboutView.vue'),
           meta: { title: 'Fangsis | 關於芳心' }
         },
         {
           path: 'products',
-          name: 'products',
           component: () => import('../views/fronted/ProductsView.vue'),
           meta: { title: 'Fangsis | 全站商品' }
         },
         {
           path: 'product/:id',
-          name: 'product',
           component: () => import('../views/fronted/ProductView.vue'),
           meta: { title: 'Fangsis | 商品資訊' }
         },
         {
           path: 'CartView',
-          name: 'CartView',
           component: () => import('../views/fronted/CartView.vue'),
           meta: { title: 'Fangsis | 購物車' }
         },
         {
           path: 'CartOrderersInformation',
-          name: 'CartOrderersInformation',
           component: () => import('../views/fronted/CartOrderersInformation.vue'),
           meta: { title: 'Fangsis | 請填寫訂購人資料' }
         },
         {
-          path: 'CartNoProduct',
-          name: 'CartNoProduct',
-          component: () => import('../views/fronted/CartNoProduct.vue'),
-          meta: { title: 'Fangsis | 購物車尚未有商品' }
+          path: 'OrderPay/:id',
+          component: () => import('../views/fronted/OrderPayView.vue'),
+          meta: { title: 'Fangsis | 進行付款' }
         },
         {
-          path: 'CartCompleteOrder',
-          name: 'CartCompleteOrder',
+          path: 'CartCompleteOrder/:id',
           component: () => import('../views/fronted/CartCompleteOrder.vue'),
-          meta: { title: 'Fangsis | 訂單建立成功' }
+          meta: { title: 'Fangsis | 完成訂單' }
         },
         {
-          name: 'CartFailedOrder',
-          path: 'CartFailedOrder',
-          component: () => import('../views/fronted/CartFailedOrder.vue'),
-          meta: { title: 'Fangsis | 訂單建立失敗' }
-        },
-        {
-          name: 'OrderSearch',
-          path: 'OrderSearch',
-          component: () => import('../views/fronted/OrderSearch.vue'),
-          meta: { title: 'Fangsis | 訂單查尋' }
-        },
-        {
-          name: 'PrivacyPolicy',
-          path: 'PrivacyPolicy',
-          component: () => import('../views/fronted/PrivacyPolicy.vue'),
-          meta: { title: 'Fangsis | 隱私權服務條款' }
-        },
-        {
-          name: 'ShoppingGuidelines',
-          path: 'ShoppingGuidelines',
-          component: () => import('../views/fronted/ShoppingGuidelines.vue'),
-          meta: { title: 'Fangsis | 購物須知' }
-        },
-        {
-          name: 'MemberLogin',
-          path: 'MemberLogin',
-          component: () => import('../views/fronted/MemberLogin.vue'),
-          meta: { title: 'Fangsis | 會員登入' }
-        },
-        {
-          name: 'MemberSignUp',
-          path: 'MemberSignUp',
-          component: () => import('../views/fronted/MemberSignUp.vue'),
-          meta: { title: 'Fangsis | 會員註冊' }
-        },
-        {
-          name: 'OrderSearch',
           path: 'OrderSearch',
           component: () => import('../views/fronted/OrderSearch.vue'),
           meta: { title: 'Fangsis | 訂單查詢' }
         },
         {
-          name: 'WishLists',
+          path: 'PrivacyPolicy',
+          component: () => import('../views/fronted/PrivacyPolicy.vue'),
+          meta: { title: 'Fangsis | 隱私權服務條款' }
+        },
+        {
+          path: 'ShoppingGuidelines',
+          component: () => import('../views/fronted/ShoppingGuidelines.vue'),
+          meta: { title: 'Fangsis | 購物須知' }
+        },
+        {
+          path: 'MemberLogin',
+          component: () => import('../views/fronted/MemberLogin.vue'),
+          meta: { title: 'Fangsis | 會員登入' }
+        },
+        {
+          path: 'MemberSignUp',
+          component: () => import('../views/fronted/MemberSignUp.vue'),
+          meta: { title: 'Fangsis | 會員註冊' }
+        },
+        {
           path: 'WishLists',
           component: () => import('../views/fronted/WishLists.vue'),
           meta: { title: 'Fangsis | 願望清單' }
@@ -113,7 +88,6 @@ const router = createRouter({
       children: [
         {
           path: 'adminProducts',
-          name: 'adminProducts',
           component: () => import('../views/admin/AdminProducts.vue'),
           meta: { title: 'Fangsis | 後台管理 - 全部產品' }
         }
