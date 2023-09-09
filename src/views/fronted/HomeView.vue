40ebfcdbb1fd93217cf4fc27cabde5a760cdca41
peihanWang <nocab7714@gmail.com>
2023-09-09 20:57:48 +0800

    feat : 移除首頁 full page vue-loading 效果

--- src/views/fronted/HomeView.vue
+++ src/views/fronted/HomeView.vue
@@ -702,27 +702,6 @@
       </VForm>
     </div>
   </section>
-  <!-- vue-loading -->
-  <loading
-    v-model:active="isLoading"
-    :can-cancel="false"
-    :lock-scroll="lockScroll"
-    :background-color="backgroundColor"
-    :opacity="opacity"
-    :is-full-page="fullPage"
-    class="vl-overlay-full-page"
-  >
-    <div class="d-flex flex-column align-items-center mx-3">
-      <img
-        class="img-fluid animate__animated animate__pulse animate__infinite"
-        src="https://storage.googleapis.com/vue-course-api.appspot.com/peihanwang-hexschool/1682598276311.png?GoogleAccessId=firebase-adminsdk-zzty7%40vue-course-api.iam.gserviceaccount.com&Expires=1742169600&Signature=qL%2F7begbYX2RvKeejMCXEVhmPIyuNGNfCCMcDFt7bJOkRpxv46JeWb2TcnzDlPj0WB%2BEdEY5kqcd%2FuGxVX0MUcXT6mdARX8Fq51Oyho%2FKl3YIiVxIVVeF7sxsMIAK0oBlZNxrvy7yzcGJLq7uUKcPuDNXzasO4M4qt9bBKXOrDkbf8%2BCMlUoWim4Q9jtjfbIO7IgAgPn5PA1DJzmhv4bfRPco8SgGxGVvG8k9Q2ZSDxnODfwqxYHJE%2Bb6woza2dFQcEzDiBR%2FVhf1%2B8%2BB2%2BWaXD5AJCkLDjLIcTtZZBO5zCfbzg2HQAknzt%2FbXAHPI3xhOJ%2F5WXn5SnRr%2F2Xk7WDhQ%3D%3D"
-        width="192"
-        height="64"
-        alt="芳心白色logo"
-      />
-      <span class="text-white fs-7 mt-2">Loading ...</span>
-    </div>
-  </loading>
 </template>
 <script>
 // Import Swiper
@@ -749,13 +728,6 @@ const Toast = Swal.mixin({
   color: '#5D7067'
 })
 
-// vue-loading
-import Loading from 'vue-loading-overlay'
-import 'vue-loading-overlay/dist/css/index.css'
-
-// animate.css
-import 'animate.css'
-
 // AOS
 import AOS from 'aos'
 import 'aos/dist/aos.css'
@@ -780,19 +752,12 @@ export default {
       },
       subscriptEmail: '',
       products: [],
-      modules: [Navigation, Autoplay, EffectFade, FreeMode], //Swiper modules
-      // vue-loading
-      isLoading: false,
-      lockScroll: true,
-      fullPage: true,
-      backgroundColor: '#5d7067',
-      opacity: 1
+      modules: [Navigation, Autoplay, EffectFade, FreeMode] //Swiper modules
     }
   },
   components: {
     Swiper,
-    SwiperSlide,
-    Loading
+    SwiperSlide
   },
   methods: {
     // 訂閱最新活動消息
@@ -806,12 +771,6 @@ export default {
   },
 
   mounted() {
-    this.isLoading = true
-    // 設定進首頁顯示 2s full page loading
-    setTimeout(() => {
-      this.isLoading = false
-    }, 2000)
-
     AOS.init({
       // Global settings:
       disable: false, // accepts following values: 'phone', 'tablet', 'mobile', boolean, expression or function
