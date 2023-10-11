53c475448d340e22fd695765b463b7f54984e638
peihanWang <nocab7714@gmail.com>
2023-10-11 21:07:18 +0800

    deploy

---
+++ assets/AdminLayout-291411bc.js
@@ -0,0 +1,1 @@
+import{_ as e,r as t,o as r,k as n}from"./index-2a0bf374.js";const s={data(){return{}}};function a(c,_,p,u,i,d){const o=t("router-view");return r(),n(o)}const m=e(s,[["render",a]]);export{m as default};
