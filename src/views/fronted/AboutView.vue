cae1b0e65db49ead0ad9522cc6a2cb95cd36e926
peihanWang <nocab7714@gmail.com>
2023-11-04 11:50:31 +0800

    fix : 關於芳心頁面排版修正
    1. 調整文字顏色為灰階色（Neutral）。
    2. 修改文字內容，並將文字標題與內容獨立排列。
    3. 限制與調整內容寬度大小。

--- src/views/fronted/AboutView.vue
+++ src/views/fronted/AboutView.vue
@@ -27,27 +27,33 @@
     </div>
   </div>
 
-  <div class="container py-13 position-relative z-index-3">
-    <div class="bkg-banai-secondary"></div>
-    <div class="bkg-smallstepsgreen-secondary"></div>
-    <div class="row justify-content-md-center">
-      <div class="col-lg-6">
-        <div class="mx-3">
-          <p>
-            <span class="division">Fangsis</span>
-            是由環保用品電商品牌「綠印生活」及花蓮在地深耕多年的優質手作品牌「白娜手作」共同創辦。創辦
-            Fangsis
-            的初衷，是為了花蓮這片土地。看到農作物受到滯銷，我們一方面覺得十分惋惜，一方面又認為一定可以做點什麼，讓花蓮甚至台灣的農產品之美可以被看到。
-          </p>
-          <p>
-            因此，我們決定用我們最擅長的方式，將滯銷農產品提煉成精油，融入在香氛商品中。我們已經與瑞穗農會達成協議，每年將固定收購文旦柚提煉的精油。未來，除了文旦柚以外，將會有更多的在地農產品被運用在香氛商品中。
-          </p>
-          <p>
-            我們希望秉持著環保愛地球的理念，並以最用心及精細的手作，而我們的使命就是<br /><span
-              class="fs-4 fw-blod"
-              >"重心出發，恢復大地原本的芳香"</span
-            >
-          </p>
+  <div class="container">
+    <div class="position-relative z-index-3 py-10 w-md-75 mx-auto">
+      <div class="bkg-banai-secondary"></div>
+      <div class="bkg-smallstepsgreen-secondary"></div>
+      <div class="row justify-content-md-center">
+        <div class="col-lg-6">
+          <div class="mx-3" style="color: #5b5c60">
+            <h3 class="fs-2 fw-bolder mb-3" style="font-family: var(--bs-NotoSerif-TC)">
+              我們的理念
+            </h3>
+            <p class="mb-7">
+              「我們希望秉持著環保愛地球的理念，並以最用心及精細的手作，重心出發，恢復大地原本的芳香。」
+            </p>
+
+            <h3 class="fs-2 fw-bolder mb-3" style="font-family: var(--bs-NotoSerif-TC)">
+              品牌起源
+            </h3>
+            <p>
+              該品牌由環保用品電商「綠印生活」及花蓮在地深耕多年的優質手作「白娜手作」共同創辦。創辦
+              Fangsis
+              的初衷，是為了花蓮這片土地。看到農作物受到滯銷，我們一方面覺得十分惋惜，一方面又認為一定可以做點什麼，讓花蓮甚至台灣的農產品之美可以被看到。
+            </p>
+            <p>
+              因此，我們決定用我們最擅長的方式，將滯銷農產品提煉成精油，融入在香氛商品中。我們已經與瑞穗農會達成協議，每年將固定收購文旦柚提煉的精油。未來，除了文旦柚以外，將會有更多的在地農產品被運用在香氛商品中。
+            </p>
+            <p></p>
+          </div>
         </div>
       </div>
     </div>
@@ -55,13 +61,6 @@
 </template>
 
 <style lang="scss" scoped>
-.division {
-  font-size: 300%;
-  float: left;
-  padding-right: 1%;
-  font-weight: bold;
-  line-height: 60%;
-}
 .bkg-banai-secondary {
   background-image: url('https://storage.googleapis.com/vue-course-api.appspot.com/peihanwang-hexschool/1690375495924.png?GoogleAccessId=firebase-adminsdk-zzty7%40vue-course-api.iam.gserviceaccount.com&Expires=1742169600&Signature=DozD4VXQB0toB564NLELKSZfCkESXl8Qtjb1hDg0ohfu5PXEx%2F1LtxOLgwhOeRzMXE0kA4F6hCAUMeF%2FYuYCKgSk%2BBPimbh%2BRqFk6g7Xa1TxR3pXVr8TahUdU3d6lI%2FoCHsPSzowU6QXbgg5PH2zaCV8HOcEoSC58WdRoduG8mavLNOa%2BWadwy29InZB0uXv75ovv3uUw2bTOwgGzka0bCKI0buB%2FnakBJZv4gy9sa6oWxZPdI6zS9M986kwKa4tQk7BwhSuQr0jAGB4nGKK%2BR06BcXBd4hImHlrbX%2F%2FCbwwVsqP7X8MPXPPihm%2Fm64nXuvMpcVQJc5zN3VpuYnbSw%3D%3D');
   background-size: auto;
