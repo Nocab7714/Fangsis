5a63888801152a9d18052f7e2225076ca2b7b006
peihanWang <nocab7714@gmail.com>
2023-11-05 11:48:32 +0800

    fix : 修改 base 設定

--- vite.config.js
+++ vite.config.js
@@ -1,15 +1,14 @@
+/* global process */
 import { fileURLToPath, URL } from 'node:url'
 
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
-/* global process */
-
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
-  // base: '/Fangsis/',
-  base: process.env.NODE_ENV === 'fangsis' ? '/fangsis/' : '/',
+  base: '/Fangsis/',
+  // base: process.env.NODE_ENV === 'Fangsis' ? '/Fangsis/' : '/',
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
