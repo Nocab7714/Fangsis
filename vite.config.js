00084cce4cfb64964f9e7cc2b1a48eedffc300be
peihanWang <nocab7714@gmail.com>
2023-11-13 15:04:04 +0800

    fix : 修改 vite.config.js 的 base 設定
    設定本地開發與編譯後的路徑，避免靜態資源的路徑取用上出現問題

--- vite.config.js
+++ vite.config.js
@@ -7,8 +7,7 @@ import vue from '@vitejs/plugin-vue'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
-  base: '/Fangsis/',
-  // base: process.env.NODE_ENV === 'Fangsis' ? '/Fangsis/' : '/',
+  base: process.env.NODE_ENV === 'production' ? '/Fangsis/' : '/',
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
